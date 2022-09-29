e42cdec61c488e683b668f4a97a8a5cdd2b64b4b
Vasia-Assembler <112320206+Vasia-Assembler@users.noreply.github.com>
2022-09-29 18:37:57 +0300

    remove usage of sort() in Задание 7

--- Задание 7/main7.js
+++ Задание 7/main7.js
@@ -1,5 +1,38 @@
+function mergeSort (arr, predicate) {
+  if (arr.length === 1) {
+    return arr
+  }
+  const middle = Math.floor(arr.length / 2)
+  const left = arr.slice(0, middle)
+  const right = arr.slice(middle)
+  return merge(
+    mergeSort(left, predicate),
+    mergeSort(right, predicate),
+    predicate
+  )
+}
+
+
+function merge (left, right, predicate) {
+  let result = []
+  let indexLeft = 0
+  let indexRight = 0
+
+  while (indexLeft < left.length && indexRight < right.length) {
+    if (predicate(left[indexLeft], right[indexRight]) <= -1) {
+      result.push(left[indexLeft])
+      indexLeft++
+    } else {
+      result.push(right[indexRight])
+      indexRight++
+    }
+  }
+
+  return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
+}
+
 function sortCitizens(input) {
-  const splitInput = input.toLowerCase().split(' ').map((e, i) => [parseFloat(e), i + 1]).sort((a, b) => a[0] - b[0])
+  const splitInput = mergeSort(input.toLowerCase().split(' ').map((e, i) => [parseFloat(e), i + 1]), (a, b) => a[0] - b[0])
   return [splitInput[0][1], splitInput[(splitInput.length % 2) + 1][1], splitInput[splitInput.length - 1][1]]
 }
 
